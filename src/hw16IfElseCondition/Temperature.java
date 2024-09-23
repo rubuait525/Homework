d804d762eeb5ba0dd18ce0e5667c4e9196a27075
Rubuait Rahman <rubuait.rahman@gmail.com>
2024-09-23 14:26:19 -0500

    Update(HW 16)

--- src/hw16IfElseCondition/Temperature.java
+++ src/hw16IfElseCondition/Temperature.java
@@ -7,7 +7,6 @@ public class Temperature {
 		System.out.println("Today's Weather");
 		Scanner scanner = new Scanner(System.in);
 		int todaysTemperature = scanner.nextInt();
-		//int temerature = scanner.nextInt();
 
 		if (todaysTemperature < 32) {
 			System.out.println("Freezing");
@@ -15,9 +14,9 @@ public class Temperature {
 			System.out.println("Pleasant");
 		} else if (todaysTemperature < 73) {
 			System.out.println("Getting Warmer");
-		} else if (todaysTemperature< 101) {
+		} else if (todaysTemperature < 101) {
 			System.out.println("Hot");
-		} else if (todaysTemperature > 101) {
+		} else if (todaysTemperature > 101 || todaysTemperature == 101) {
 			System.out.println("Vary Hot");
 		} else {
 			System.out.println("Please put a valid temperature");
