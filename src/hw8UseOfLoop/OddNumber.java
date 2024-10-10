3a0efa7a76832f2458d9148cb2c5a7564c993d11
Rubuait Rahman <rubuait.rahman@gmail.com>
2024-10-10 17:29:07 -0500

    Hw8UseOfLoop

---
+++ src/hw8UseOfLoop/OddNumber.java
@@ -0,0 +1,12 @@
+package hw8UseOfLoop;
+
+public class OddNumber {
+	public static void main(String[] args) {
+		for(int i =-20; i<=50;i++) {
+			if (i % 2 != 0) {
+				System.out.println("Odd Number: " + i);
+			}
+		}
+	}
+
+}
